1c89cff431c195ebfa526911c540904538e8079f
r-tanya <80388239+r-tanya@users.noreply.github.com>
2021-07-10 23:34:03 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,19 +1,19 @@
 
 CREATE TABLE Publishing_House
 (
-  pub_id INT NOT NULL,
+  pub_id INT AUTO_INCREMENT NOT NULL,
   pub_name VARCHAR(30) NOT NULL,
-  year_started INT NOT NULL,
+  year_started INT,
   PRIMARY KEY (pub_id)
 );
 
 CREATE TABLE Author
 (
-  author_id INT NOT NULL,
+  author_id INT AUTO_INCREMENT NOT NULL,
   author_name VARCHAR(30) NOT NULL,
-  author_birth_date DATE NOT NULL,
-  author_country CHAR(30) NOT NULL,
-  PRIMARY KEY (author_id)
+  author_birth_date DATE,
+  author_country CHAR(30),
+  PRIMARY KEY (author_id) 
 );
 
 CREATE TABLE Genre
@@ -97,22 +97,22 @@ INSERT INTO publishing_house VALUES (1, "Macmillan", "1843");
 INSERT INTO publishing_house VALUES (2, "HarperCollins", "1989");
 INSERT INTO author VALUES (1, "Lewis Carroll", "1832-01-27", "United Kingdom");
 INSERT INTO author VALUES (2, "C.S. Lewis", "1898-11-22", "United Kingdom");
-INSERT INTO genre VALUES ("Children’s", "A genre produced for the entertainment of children and young adults.");
+INSERT INTO genre VALUES ("Children", "A genre produced for the entertainment of children and young adults.");
 INSERT INTO genre VALUES ("Fantasy", "A genre that typically involves the use of magic or witchcraft or other supernatural elements, often inspired by real world folklore and mythology.");
 INSERT INTO salesperson VALUES (1, "Benvolio Benjamin", 15000, "1992-12-12");
 INSERT INTO salesperson VALUES (2, "Tybalt Tyler", 15000, "1998-03-29");
-INSERT INTO book VALUES (1, "9781447279990", "Alice’s Adventures in Wonderland", "1865-11-26", 146, 210, 50, 1);
+INSERT INTO book VALUES (1, "9781447279990", "Alice's Adventures in Wonderland", "1865-11-26", 146, 210, 50, 1);
 INSERT INTO book VALUES (2, "9780064404990", "The Lion, the Witch, and the Wardrobe", "1950-10-16", 208, 270, 60, 2);
-INSERT INTO book VALUES (3, "9780064471107", "The Magician’s Nephew", "1955-05-02", 183, 270, 40, 2);
+INSERT INTO book VALUES (3, "9780064471107", "The Magician's Nephew", "1955-05-02", 183, 270, 40, 2);
 INSERT INTO book_in_series VALUES (2, "The Chronicles of Narnia", 2);
-INSERT INTO book_in_series VALUES (3, "The Magician’s Nephew", 1);
+INSERT INTO book_in_series VALUES (3, "The Magician's Nephew", 1);
 INSERT INTO book_award VALUES (1, 1988, "Kurt Maschler Award");
 INSERT INTO author_book VALUES (1, 1);
 INSERT INTO author_book VALUES (2, 2);
 INSERT INTO author_book VALUES (2, 3);
-INSERT INTO book_genre VALUES ("Children’s", 1);
-INSERT INTO book_genre VALUES ("Children’s", 2);
-INSERT INTO book_genre VALUES ("Children’s", 3);
+INSERT INTO book_genre VALUES ("Children", 1);
+INSERT INTO book_genre VALUES ("Children", 2);
+INSERT INTO book_genre VALUES ("Children", 3);
 INSERT INTO book_genre VALUES ("Fantasy", 1);
 INSERT INTO book_genre VALUES ("Fantasy", 2);
 INSERT INTO book_genre VALUES ("Fantasy", 3);
